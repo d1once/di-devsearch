42cdb60bf855e5ba23adefb179ba174eabd028b4
Dionis <dionisenja@gmail.com>
2021-12-20 12:22:26 +0100

    databases

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import Project
 # Create your views here.
 
 
 def kot_view(request):
+    projects = Project.objects.all()
     message = 'GTFO MUDA FUCKA!!!!'
     number = 5
-    context = {"kot": "GTFO", 'number': number}
+    context = {"kot": message, 'number': number, "projects": projects}
     return render(request, 'projects/kot.html', context)
